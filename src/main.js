7b7164e18bd9b7a851aa942799bd51fcc3cc3703
Carlos Medina <xziolhvinvhgl@gmail.com>
2017-11-25 18:41:05 -0400

    Terminando proyecto, primer paso

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
-console.log("HELLO UNIVERSE!!");
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class MyApp extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      lastName: ''
+    };
+  }
+
+  handleName = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleLastName = (e) => {
+    this.setState({
+      lastName: e.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Hello Universe from react!!</h4>
+        <div>
+          <label>Name:</label>
+          <input type='text' onChange={e => this.handleName(e)} />
+          <label>Apellido:</label>
+          <input type='text' onChange={e => this.handleLastName(e)} />
+        </div>
+        <div>
+          {this.state.name} {this.state.lastName}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+ReactDOM.render(<MyApp />, document.getElementById('my-app'));
